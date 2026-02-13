56235d0caf332b0e7657c7fe15747965d7636c8e
pooja-zagade <you@example.com>
2026-02-13 12:43:19 +0530

    This is a second chance

--- Script2.sh
+++ Script2.sh
@@ -6,4 +6,5 @@ echo "Value of b"
 read b
 echo "Hello value of a is $a and value of b is $b"
 echo "we new it this data from the user"
-echo "We are trying this practical from Linux EC2 instance"
+echo "We are trying this practical from Linux EC2 instance" git cd "
+echo "I am from ygmind"
